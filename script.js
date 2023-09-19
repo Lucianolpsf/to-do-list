3c6a1b0ee53072bf79e86b677680e68e6a65d0b1
Luciano Lopes <Lucianolpsf@gmail.com>
2023-09-19 11:17:34 -0300

    Incluindo localstorage para salvar e excluir tarefas

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const btnAdicionar = document.getElementById('adicionar')
-const valorTarefa = document.getElementById('texto-input')
+const textoTarefa = document.getElementById('texto-input')
+let tarefasSalvas = []
+
+if (localStorage.getItem('tarefasSalvas')){renderizarTarefa()}
 
 btnAdicionar.addEventListener('click', (evento)=>{
     evento.preventDefault();
 
-    if (valorTarefa.value == ''){
+    if (textoTarefa.value == ''){
         alert('Digite uma tarefa valida')
     } else {
-        const tarefa =  criarTarefa(valorTarefa.value)
-        renderizarTarefa(tarefa)
-    }
-    
-    valorTarefa.value = ''
-
+        salvarTarefa(textoTarefa.value)
+        renderizarTarefa()
+    }   
+    textoTarefa.value = ''
 })
 
 const listaTarefas = document.querySelector('ul')
@@ -20,9 +21,13 @@ const listaTarefas = document.querySelector('ul')
 listaTarefas.addEventListener('click',(elemento) =>{
 
     const itemClicado = elemento.target 
-
+ 
     if (itemClicado.classList.contains('excluir')){
-       itemClicado.parentElement.parentElement.remove()
+       
+        let tarefa = itemClicado.parentElement.parentElement.firstElementChild.innerText
+
+        excluirTarefa(tarefa)
+        renderizarTarefa()
     }
 
     if (itemClicado.classList.contains('concluir')){
@@ -30,22 +35,53 @@ listaTarefas.addEventListener('click',(elemento) =>{
     }
 })
 
+function excluirTarefa(tarefa){
 
-function criarTarefa (valorTarefa){
-    const li = document.createElement('li')
+    tarefasSalvas = JSON.parse(localStorage.getItem('tarefasSalvas'))
 
-    li.innerHTML = `
-    <p>${valorTarefa}</p> 
-    <div>
-        <button class="excluir"></button>
-        <button class="concluir"></button>
-    </div>`
+    let indice = tarefasSalvas.indexOf(tarefa)
 
-    return li
+    tarefasSalvas.splice(indice, 1)
+
+    localStorage.setItem('tarefasSalvas',JSON.stringify(tarefasSalvas))
+
 }
 
-function renderizarTarefa(tarefa){
+function renderizarTarefa(){
     const listaTarefas = document.querySelector('ul')
 
-    listaTarefas.appendChild(tarefa)
+    listaTarefas.innerHTML = '' 
+
+    tarefasSalvas = JSON.parse(localStorage.getItem('tarefasSalvas'))
+
+    tarefasSalvas.forEach(tarefa => {
+
+        const li = document.createElement('li')
+
+        li.innerHTML = `
+        <p>${tarefa}</p> 
+        <div>
+            <button class="excluir"></button>
+            <button class="concluir"></button>
+        </div>`
+
+        listaTarefas.appendChild(li)
+    });
+}
+
+function salvarTarefa (textoTarefa){
+    if (localStorage.getItem('tarefasSalvas')){
+        tarefasSalvas = JSON.parse(localStorage.getItem('tarefasSalvas'))
+        tarefasSalvas.push(textoTarefa)
+        localStorage.setItem('tarefasSalvas',JSON.stringify(tarefasSalvas))
+    }
+    else
+    {
+        tarefasSalvas.push(textoTarefa)
+        localStorage.setItem('tarefasSalvas',JSON.stringify(tarefasSalvas))
+    }
+}
+
+function log(mensagem){
+    console.log(mensagem)
 }
